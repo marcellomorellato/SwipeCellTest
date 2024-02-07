a7762ec6f3fe8bad27b52beb679572a5b33c55e8
marcello <m.morellato@ferlini.it>
2024-02-07 15:00:08 +0100

    Initial Commit

---
+++ SwipeCellTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwipeCellTest
+//
+//  Created by Marcello Morellato on 07/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
